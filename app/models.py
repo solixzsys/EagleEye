8eff942c484a6985f1abd316a55a5568d4a90c6c
soliu <solixzsystem@gmail.com>
2015-11-11 21:08:52 +0100

    Addition of Channel

--- app/models.py
+++ app/models.py
@@ -14,5 +14,11 @@ class Country(models.Model):
         self.country_code=str(self.country_name).replace(" ","_")
         super(Country,self).save(*args,**kwargs)
 
+class Channel(models.Model):
+    channel_id=models.CharField(max_length=200,primary_key=True,default=str(datetime.datetime.now()))
+    channel_name=models.CharField(max_length=200)
+    country_id=models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.country_code
 
