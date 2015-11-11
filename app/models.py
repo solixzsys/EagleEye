7c703eac985eaedeb1f8ca57fae3d452f9ea2196
soliu <solixzsystem@gmail.com>
2015-11-11 20:28:13 +0100

    First EagleEye Commit with only Country model

---
+++ app/models.py
@@ -0,0 +1,18 @@
+
+from django.db import models
+import datetime
+
+class Country(models.Model):
+    country_code=models.CharField(max_length=200,primary_key=True,default=str(datetime.datetime.now()))
+    country_name=models.CharField(max_length=200)
+    continent=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.country_code
+
+    def save(self,*args,**kwargs):
+        self.country_code=str(self.country_name).replace(" ","_")
+        super(Country,self).save(*args,**kwargs)
+
+
+
